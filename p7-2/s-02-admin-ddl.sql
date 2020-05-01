c1b4361a3e1848ea591acc262fff7e4f685d457f
DragoPyro <tryper30@gmail.com>
2020-04-30 20:56:42 -0500

    agregue archivo 2/p7

---
+++ p7-2/s-02-admin-ddl.sql
@@ -0,0 +1,7 @@
+--@Autor(es):       Eliezer Jair Ochoa Santos, 
+--@Fecha creación:  27/04/2020
+--@Descripción:     Creación de usuarios y roles 
+
+
+prompt Proporcionar el password del usuario sys:
+connect sys as sysdba 
