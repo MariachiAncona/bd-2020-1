97229472668f299097bd616c476cb6de1af93766
Simón Ramírez <sera.-@hotmail.com>
2020-04-30 21:14:09 -0500

    Cambios menores en archivo 1 práctica 7

--- p7-2/s-01-usuarios.sql
+++ p7-2/s-01-usuarios.sql
@@ -1,4 +1,4 @@
---@Autor(es):       Eliezer Jair Ochoa Santos, 
+--@Autor(es):       Eliezer Jair Ochoa Santos, Ramírez Ancona Simón Eduardo
 --@Fecha creación:  27/04/2020
 --@Descripción:     Creación de usuarios y roles 
 
@@ -21,36 +21,23 @@ prompt dando permisos al rol p0701_guest_rol:
 grant create session, create synonym to p0701_guest_rol;
 
 
-
 prompt creando usuario jos_p0701_admin:
 create user jos_p0701_admin identified by jair
 quota 1024m on users;
 prompt asignando el rol de p0701_admin_rol:
-grant p0701_admin_rol to jos_p0701_admin;
+grant p0701_admin_rol, create procedure, create sequence to jos_p0701_admin;
 
 
 prompt creando usuario jos_p0701_oper:
 create user jos_p0701_oper identified by jair
 quota 500m on users;
 prompt asignando el rol de p0701_basic_rol:
-grant p0701_basic_rol to jos_p0701_oper;
+grant p0701_basic_rol,create procedure, create sequence to jos_p0701_oper;
 
 prompt creando usuario jos_p0701_invitado:
 create user jos_p0701_invitado identified by jair;
 prompt asignando el rol de p0701_guest_rol:
-grant p0701_guest_rol to jos_p0701_invitado;
-
-
-
-prompt dando permisos al usuario jos_p0701_admin:
-grant create procedure, create sequence to jos_p0701_admin;
-
-prompt dando permisos al usuario jos_p0701_oper:
-grant create procedure, create sequence to jos_p0701_oper;
-
-prompt dando permisos al usuario jos_p0701_invitado:
-grant create procedure, create sequence to jos_p0701_invitado;
-
+grant p0701_guest_rol, create procedure, create sequence to jos_p0701_invitado;
 
 prompt LISTO!
 disconnect;
