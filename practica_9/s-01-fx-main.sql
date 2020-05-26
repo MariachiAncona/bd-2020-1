efa7ee4fcac22794bbba27c3b9a44d3e0ade577d
DragoPyro <tryper30@gmail.com>
2020-05-26 15:05:41 -0500

    cadena

--- practica_9/s-01-fx-main.sql
+++ practica_9/s-01-fx-main.sql
@@ -6,13 +6,14 @@ prompt Conectando como sys para crear usuarios
 connect sys as sysdba
 
 prompt Creando usuario josr_p0903_fx
-create user josr_p0802_cuentas identified by josr
+create user josr_p0903_fx identified by josr
 quota unlimited on users;
 
 prompt Asignando privilegios a josr_p0903_fx;
 grant create session, create procedure, create sequence, create table, insert any table
 to josr_p0903_fx;
 
+prompt Conectando como josr_p0903_fx para crear tablas
 connect josr_p0903_fx/josr 
 
 start s-02-fx-ddl.sql 
