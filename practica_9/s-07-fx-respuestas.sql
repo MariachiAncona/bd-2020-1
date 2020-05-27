b311b858a2bd4d3f605561b59b14b12e4d9fb581
Simón Ramírez <sera.-@hotmail.com>
2020-05-27 17:36:31 -0500

    Archivo respuestas completo

--- practica_9/s-07-fx-respuestas.sql
+++ practica_9/s-07-fx-respuestas.sql
@@ -4,16 +4,16 @@
 
 create table consulta_1 as
     select id, nombre, clave, municipio,
-        to_char(ultima_revision,'dd/mm/yyyy hh24:mi:ss')|| ' hrs.' as
-        ULTIMA_REVISION
+        to_char(ultima_revision,'dd/mm/yyyy hh24:mi:ss')
+        || ' hrs.' as ULTIMA_REVISION
     from aeropuerto
     where tipo=lower('closed') and
-        ultima_revision between to_date('01/10/2012','dd/mm/yyyy') and
-        to_date('31/03/2015','dd/mm/yyyy');
+        ultima_revision between to_date('01/10/2012','dd/mm/yyyy') 
+        and to_date('31/03/2015','dd/mm/yyyy');
 
 create table consulta_2 as
-    select id, nombre, municipio, region_iso, elevacion*(3.281)
-        as ELEVACION_METROS
+    select id, nombre, municipio, region_iso, 
+        elevacion*(3.281) as ELEVACION_METROS
     from aeropuerto 
     where tipo= 'large_airport';    
 
@@ -23,7 +23,7 @@ create table consulta_3 as
         abs(trunc(longitud*(10002.29/90.00),4)) as LONG_CARTESIANA,
         trunc(latitud,4) as LATITUD_GRADOS, trunc(longitud,4) as LONGITUD_GRADOS
     from aeropuerto 
-    where region_iso=('MX-OAX');
+    where region_iso=upper('mx-oax');
 
 
 --Pendiente
@@ -31,31 +31,32 @@ create table consulta_4 as
     select a2.nombre, a2.tipo, a1.latitud as LATITUD_BENITO, 
         a1.longitud as LONGITUD_BENITO, a2.latitud as LATITUD_OTRO,
         a2.longitud as LONGITUD_OTRO,
-        trunc(
+        round(
             sqrt(power(a1.latitud*(10002.29/90.00)-a2.latitud*(10002.29/90.00),2) 
                 +power(a1.longitud*(10002.29/90.00)-a2.longitud*(10002.29/90.00),2))
             , 5) as DISTANCIA
     from aeropuerto a1, aeropuerto a2
-    where a1.id = 4731 and a2.region_iso = 'MX-DIF'
+    where a1.id = 4731 and a2.region_iso = upper('mx-dif')
         and a2.id <> 4731 and a2.tipo <> 'closed'
         order by distancia asc;
 
 
 create table consulta_5 as
     select id, clave, nombre, municipio, codigo_gps, codigo_iata,
-        to_char(ultima_revision, 'FMday"," month dd "of" yyyy "at" hh24:mi:ss')
-        as ULTIMA_REVISION
+        to_char(ultima_revision,'FMday"," month dd "of" yyyy "at" hh24:mi:ss') 
+            as ULTIMA_REVISION
     from aeropuerto
-    where region_iso = 'MX-CHP';
+    where region_iso = upper('mx-chp');
 
 
 --Cambiando idioma
 alter session set nls_language=spanish;
-
 create table consulta_6 as
     select id,
-        to_char(ultima_revision, 'dd/FMmonth/yyyy') as ULTIMA_REVISION,
-        to_date('01/01/2018', 'dd/mm/yy') - TRUNC(ultima_revision) AS FALTAN
+        to_char(ultima_revision, 'dd/FMmonth/yyyy') 
+            as ULTIMA_REVISION,
+        to_date('01/01/2018', 'dd/mm/yy') - 
+            trunc(ultima_revision) as FALTAN
     from aeropuerto
     where to_char(ultima_revision, 'dd/mm') = '10/12' or
             to_char(ultima_revision, 'dd/mm') = '15/12'
@@ -65,20 +66,20 @@ alter session set nls_language=american;
 
 
 create table consulta_7 as
-    select unique decode(upper(tipo),
-            'HELIPORT',  'H',
-            'CLOSED',           'C',
-            'SMALL_AIRPORT',    'S',
-            'LARGE_AIRPORT',    'L',
-            'MEDIUM_AIRPORT',   'M', 
-            'SEAPLANE_BASE',    'B') as CLAVE_TIPO,
-        UPPER(tipo) as TIPO
+    select  decode(upper(tipo),
+            upper('heliport'),'H',
+            upper('closed'),'C',
+            upper('small_airport'),'S',
+            upper('large_airport'),'L',
+            upper('medium_airport'),'M', 
+            upper('seaplane_base'),'B') as CLAVE_TIPO,
+        upper(tipo) as TIPO
     from aeropuerto;
 
 create table consulta_8 as
     select to_char(LPAD(id, 6, '0')||'-'
-            || SUBSTR(region_iso,4, 3)|| '-'
-            || SUBSTR(UPPER(municipio), (LENGTH(UPPER(municipio)) - 1), 2)
+            || substr(region_iso,4, 3)|| '-'
+            || substr(upper(municipio), (length(upper(municipio)) - 1), 2)
             ) as FOLIO,
         region_iso, municipio, wikipedia_link
     from aeropuerto
@@ -86,11 +87,10 @@ create table consulta_8 as
 
 create table consulta_9 as
     select to_char(LPAD(id, 6, '0')|| '-'
-            || SUBSTR(region_iso,4, 3)|| '-'
-            || SUBSTR(
-                    NVL(UPPER(municipio), 'NNNN'),
-                    (LENGTH( NVL(UPPER(municipio), 'NNNN') ) - 1)
-                    , 2)
+            || substr(region_iso,4, 3)|| '-'
+            || substr(nvl(upper(municipio), 'NNNN'),
+                    (length( nvl(upper(municipio), 'NNNN') ) - 1)
+                , 2)
             ) as FOLIO,
         region_iso, municipio, wikipedia_link
         from aeropuerto
@@ -99,12 +99,12 @@ create table consulta_9 as
 create table consulta_10 as
     select nombre,
         pagina_web,
-        SUBSTR(
+        substr(
             pagina_web,
             nullif(
                 instr(pagina_web, '?', 1, 1),0
             ) + 1,
-            LENGTH(pagina_web)
+            length(pagina_web)
         ) as PARAMETROS
     from aeropuerto
     where pagina_web is not null;
@@ -118,4 +118,4 @@ create table consulta_11 as
                 instr(wikipedia_link, '/', 1, 4), length(wikipedia_link)))
         ,0) as TOTAL_LONGITUD
     from aeropuerto
-    where tipo = 'small_airport' and region_iso = 'MX-BCS';
+    where tipo = 'small_airport' and region_iso = upper('mx-bcs');
