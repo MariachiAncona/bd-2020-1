7950730f54f2e85f58bcf8a2bf5a780905e73714
Simón Ramírez <sera.-@hotmail.com>
2020-05-27 00:23:47 -0500

    eje 4

--- practica_9/s-07-fx-respuestas.sql
+++ practica_9/s-07-fx-respuestas.sql
@@ -24,3 +24,15 @@ select nombre, trunc(latitud*(10002.29/90.00),4) as LAT_CARTESIANA,
     trunc(latitud,4) as LATITUD_GRADOS, trunc(longitud,4) as LONGITUD_GRADOS
 from aeropuerto 
 where region_iso=('MX-OAX');
+
+select a2.nombre, a2.tipo, a1.latitud as LATITUD_BENITO, a1.longitud as LONGITUD_BENITO, a2.latitud as LATITUD_OTRO,
+ a2.longitud as LONGITUD_OTRO, 
+ TRUNC(SQRT(POWER(TRUNC(ABS( (a1.latitud/90) * 10002.29 ), 5)-TRUNC(ABS( (a2.latitud/90) * 10002.29 ), 5),2) 
+ +POWER(TRUNC(ABS( (a1.longitud/90) * 10002.29), 5)-TRUNC(ABS( (a2.longitud/90) * 10002.29), 5),2)), 5) AS DISTANCIA
+from aeropuerto a1, aeropuerto a2
+WHERE a1.id = 4731          -- aeropuerto Benito Juárez.
+    AND a2.region_iso = 'MX-DIF'
+    AND a2.id <> 4731           -- a2 no debe ser el mismo aeropuerto Benito Juarez ya que la
+                                -- distancia sería cero.
+    AND a2.tipo <> 'closed'     -- excluir a los aeropuertos cerrados.
+    ORDER BY distancia ASC;
